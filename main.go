9623295ad981876af02b7086bee6ec4c7d2a8008
Jacob <jacobroe@rogers.com>
2025-01-02 23:05:27 -0500

    Removed example implementations

--- main.go
+++ main.go
@@ -2,14 +2,10 @@ package main
 
 import (
 	"discordBot/data"
-	"encoding/binary"
-	"fmt"
-	"io"
 	"os"
 	"os/signal"
 	"strings"
 	"syscall"
-	"time"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -26,11 +22,7 @@ func main() {
 		panic(err)
 	}
 
-	loadAudio()
-
 	dgSession.AddHandler(messageHandler)
-	dgSession.AddHandler(userHandler)
-	dgSession.AddHandler(presenceHandler)
 	dgSession.AddHandler(ready)
 
 	dgSession.Identify.Intents = discordgo.IntentsAll
@@ -61,35 +53,6 @@ func messageHandler(session *discordgo.Session, message *discordgo.MessageCreate
 		return
 	}
 
-	if message.Content == "Hello" {
-		session.ChannelMessageSend(message.ChannelID, "World!")
-		fmt.Println(message.ChannelID)
-	}
-
-	if message.Content == "FN" {
-		session.ChannelMessageSend(message.ChannelID, "BR")
-	}
-
-	if message.Content == "Join" {
-		channel, err := session.State.Channel(message.ChannelID)
-		if err != nil {
-			panic(err)
-		}
-
-		guild, err := session.State.Guild(channel.GuildID)
-		if err != nil {
-			panic(err)
-		}
-
-		for _, vs := range guild.VoiceStates {
-			if vs.UserID == message.Author.ID {
-				err = playAudio(session, guild.ID, vs.ChannelID)
-				if err != nil {
-					panic(err)
-				}
-			}
-		}
-	}
 	if after, result := strings.CutPrefix(message.Content, ".link "); result {
 		anilistID := searchUserIDByName(after)
 		_, err := data.CreateUser(message.Author.ID, anilistID)
@@ -99,69 +62,4 @@ func messageHandler(session *discordgo.Session, message *discordgo.MessageCreate
 			session.ChannelMessageSend(message.ChannelID, "Account Linked!")
 		}
 	}
-}
-
-func userHandler(session *discordgo.Session, user *discordgo.GuildMemberAdd) {
-	session.ChannelMessageSend(MainChannelID, fmt.Sprintf("Welcome %s", user.DisplayName()))
-}
-
-func presenceHandler(session *discordgo.Session, presence *discordgo.PresenceUpdate) {
-	session.ChannelMessageSend(MainChannelID, fmt.Sprintf("%s %s", presence.User.Username, presence.Status))
-}
-
-func loadAudio() error {
-
-	file, err := os.Open("airhorn.dca")
-	if err != nil {
-		fmt.Println("Error opening sound file.")
-		return err
-	}
-
-	var opusLen int16
-	for {
-		err = binary.Read(file, binary.LittleEndian, &opusLen)
-		if err == io.EOF || err == io.ErrUnexpectedEOF {
-			err := file.Close()
-			if err != nil {
-				return err
-			}
-			return nil
-		}
-
-		if err != nil {
-			fmt.Println("Error reading dca file.")
-			return err
-		}
-
-		inBuf := make([]byte, opusLen)
-		err := binary.Read(file, binary.LittleEndian, &inBuf)
-		if err != nil {
-			fmt.Println("Error reading dca file.")
-			return err
-		}
-		AudioBuffer = append(AudioBuffer, inBuf)
-	}
-}
-
-func playAudio(session *discordgo.Session, guildID string, channelID string) error {
-	vc, err := session.ChannelVoiceJoin(guildID, channelID, false, true)
-	if err != nil {
-		return err
-	}
-
-	time.Sleep(250 * time.Millisecond)
-
-	vc.Speaking(true)
-
-	for _, buff := range AudioBuffer {
-		vc.OpusSend <- buff
-	}
-
-	vc.Speaking(false)
-
-	time.Sleep(250 * time.Millisecond)
-
-	vc.Disconnect()
-
-	return nil
 }
